7e0b00b758384468eccd5d29973a50b82c1d95fc
raphdsz <raphdsz@gmail.com>
2022-06-13 18:23:37 +0800

    v1.5

--- app2.js
+++ app2.js
@@ -3,6 +3,8 @@ var synth= new Tone.PolySynth().toMaster();
 const now = Tone.now();
 let startingTouchX;
 let startingTouchY;
+let eventTouchArray=[];
+
 
 
 
@@ -132,40 +134,159 @@ window.addEventListener("resize",()=>{
 
 
 
+function addTouchLength(){
+    eventTouchArray.push(event.touches.length);
+}
+function changeTouchLength(){
+    if (eventTouchArray[eventTouchArray.length] != eventTouchArray[eventTouchArray.length-1]){
+        startingTouchY=event.touches[0].clientY;
+        startingTouchX=event.touches[0].clientX;
+    }
+}
 
 
-
+// padsContainer.addEventListener("mousedown",playNote);
+padsContainer.addEventListener("touchstart",playNote);
+padsContainer.addEventListener("touchend",reset);
+padsContainer.addEventListener("touchmove",detune);
+padsContainer.addEventListener("touchstart",addTouchLength);
+padsContainer.addEventListener("touchend",changeTouchLength);
 
 
 
 (function(){
-    console.log('show-touch-js loaded!');
-    var svg_body = 'data:image/svg+xml,%3Csvg width=\'200\' height=\'200\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg%3E%3Cellipse opacity=\'0.65\' ry=\'50\' rx=\'50\' id=\'svg_1\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23000\' fill=\'%23000\'/%3E%3Cellipse opacity=\'0.55\' ry=\'35\' rx=\'35\' id=\'svg_2\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23FFF\' fill=\'%23FFF\'/%3E%3C/g%3E%3C/svg%3E';
-    img = document.createElement('img');
-    img.src = svg_body;
-    img.style.width = '3em';
-    img.style.height = '3em';
-    img.style.display = 'none';
-    img.style.position = 'absolute';
+    // var svg_body = 'data:image/svg+xml,%3Csvg width=\'200\' height=\'200\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg%3E%3Cellipse opacity=\'0.65\' ry=\'50\' rx=\'50\' id=\'svg_1\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23000\' fill=\'%23000\'/%3E%3Cellipse opacity=\'0.55\' ry=\'35\' rx=\'35\' id=\'svg_2\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23FFF\' fill=\'%23FFF\'/%3E%3C/g%3E%3C/svg%3E';
+    // var img = document.createElement('img');
+    // img.classList.add("pointer")
+    // img.src = svg_body;
+    // var img3 = document.createElement('img');
+    // img3.classList.add("pointer")
+    // img3.src = svg_body;
+
+    var pointer = document.querySelectorAll('.pointer');
+
+
+
     document.addEventListener("DOMContentLoaded", function() {
-        document.body.append(img);
         var showButtonHandler = function(event){
-            if (event.touches.length>1){
-                img.style.display = 'none';
-            }else{
-            img.style.display = '';
-            img.style.left = 'calc(' + event.touches[0].clientX + 'px - 1.5em)';
-            img.style.top = 'calc(' + event.touches[0].clientY + 'px - 1.5em)';
-            }
+
+            pointer[0].style.visibility = 'visible';  
+            pointer[0].style.opacity = '1';
+            pointer[0].style.left = 'calc(' + event.touches[0].clientX + 'px - 1.5em)';
+            pointer[0].style.top = 'calc(' + event.touches[0].clientY + 'px - 1.5em)';
+
+            if (event.touches.length===2){
+                pointer[1].style.visibility = 'visible';  
+                pointer[1].style.opacity = '1';
+                pointer[1].style.left = 'calc(' + event.touches[1].clientX + 'px - 1.5em)';
+                pointer[1].style.top = 'calc(' + event.touches[1].clientY + 'px - 1.5em)';
+        }
+            if (event.touches.length===3){
+                pointer[2].style.visibility = 'visible';  
+                pointer[2].style.opacity = '1';
+                pointer[2].style.left = 'calc(' + event.touches[2].clientX + 'px - 1.5em)';
+                pointer[2].style.top = 'calc(' + event.touches[2].clientY + 'px - 1.5em)';
+
+                pointer[1].style.visibility = 'visible';  
+                pointer[1].style.opacity = '1';
+                pointer[1].style.left = 'calc(' + event.touches[1].clientX + 'px - 1.5em)';
+                pointer[1].style.top = 'calc(' + event.touches[1].clientY + 'px - 1.5em)';
+
+        }
+            if (event.touches.length===4){
+                pointer[3].style.visibility = 'visible';  
+                pointer[3].style.opacity = '1';
+                pointer[3].style.left = 'calc(' + event.touches[3].clientX + 'px - 1.5em)';
+                pointer[3].style.top = 'calc(' + event.touches[3].clientY + 'px - 1.5em)';
+
+                pointer[2].style.visibility = 'visible';  
+                pointer[2].style.opacity = '1';
+                pointer[2].style.left = 'calc(' + event.touches[2].clientX + 'px - 1.5em)';
+                pointer[2].style.top = 'calc(' + event.touches[2].clientY + 'px - 1.5em)';
+
+                pointer[1].style.visibility = 'visible';  
+                pointer[1].style.opacity = '1';
+                pointer[1].style.left = 'calc(' + event.touches[1].clientX + 'px - 1.5em)';
+                pointer[1].style.top = 'calc(' + event.touches[1].clientY + 'px - 1.5em)';
+
+        }
+            if (event.touches.length===5){
+                pointer[4].style.visibility = 'visible';  
+                pointer[4].style.opacity = '1';
+                pointer[4].style.left = 'calc(' + event.touches[4].clientX + 'px - 1.5em)';
+                pointer[4].style.top = 'calc(' + event.touches[4].clientY + 'px - 1.5em)';
+
+                pointer[3].style.visibility = 'visible';  
+                pointer[3].style.opacity = '1';
+                pointer[3].style.left = 'calc(' + event.touches[3].clientX + 'px - 1.5em)';
+                pointer[3].style.top = 'calc(' + event.touches[3].clientY + 'px - 1.5em)';
+
+                pointer[2].style.visibility = 'visible';  
+                pointer[2].style.opacity = '1';
+                pointer[2].style.left = 'calc(' + event.touches[2].clientX + 'px - 1.5em)';
+                pointer[2].style.top = 'calc(' + event.touches[2].clientY + 'px - 1.5em)';
+
+                pointer[1].style.visibility = 'visible';  
+                pointer[1].style.opacity = '1';
+                pointer[1].style.left = 'calc(' + event.touches[1].clientX + 'px - 1.5em)';
+                pointer[1].style.top = 'calc(' + event.touches[1].clientY + 'px - 1.5em)';
+
+        }
+            
         };
         pads.forEach(pad =>{
             pad.addEventListener("touchmove", showButtonHandler);
             pad.addEventListener("touchstart", showButtonHandler);
             pad.addEventListener("touchend", function(event){
-                img.style.display = 'none';
+            pointer[0].style.visibility = 'hidden';
+            pointer[0].style.opacity = '0';
+            pointer[1].style.visibility = 'hidden';
+            pointer[1].style.opacity = '0';
+            pointer[2].style.visibility = 'hidden';
+            pointer[2].style.opacity = '0';
+            pointer[3].style.visibility = 'hidden';
+            pointer[3].style.opacity = '0';
+            pointer[4].style.visibility = 'hidden';
+            pointer[4].style.opacity = '0';
             });
-    
         });
     });
 })();
+
+
+
+
+
+
+
+// (function(){
+//     console.log('show-touch-js loaded!');
+//     var svg_body = 'data:image/svg+xml,%3Csvg width=\'200\' height=\'200\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg%3E%3Cellipse opacity=\'0.65\' ry=\'50\' rx=\'50\' id=\'svg_1\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23000\' fill=\'%23000\'/%3E%3Cellipse opacity=\'0.55\' ry=\'35\' rx=\'35\' id=\'svg_2\' cy=\'100\' cx=\'100\' stroke-width=\'100\' stroke=\'%23FFF\' fill=\'%23FFF\'/%3E%3C/g%3E%3C/svg%3E';
+//     img = document.createElement('img');
+//     img.src = svg_body;
+//     img.style.width = '3em';
+//     img.style.height = '3em';
+//     img.style.display = 'none';
+//     img.style.position = 'absolute';
+//     document.addEventListener("DOMContentLoaded", function() {
+//         document.body.append(img);
+//         var showButtonHandler = function(event){
+//             if (event.touches.length>1){
+//                 img.style.display = 'none';
+//             }else{
+//             img.style.display = '';
+//             img.style.left = 'calc(' + event.touches[0].clientX + 'px - 1.5em)';
+//             img.style.top = 'calc(' + event.touches[0].clientY + 'px - 1.5em)';
+//             }
+//         };
+//         pads.forEach(pad =>{
+//             pad.addEventListener("touchmove", showButtonHandler);
+//             pad.addEventListener("touchstart", showButtonHandler);
+//             pad.addEventListener("touchend", function(event){
+//                 img.style.display = 'none';
+//             });
+    
+//         });
+//     });
+// })();
 
