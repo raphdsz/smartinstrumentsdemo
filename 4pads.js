de1d058950636c480ab54ceabf0da9c30fe7f07c
raphdsz <raphdsz@gmail.com>
2022-06-23 17:21:38 +0800

    Update 4pads.js

--- 4pads.js
+++ 4pads.js
@@ -316,7 +316,7 @@ gsharpKey.addEventListener("touchend",function(e){
         key.classList.remove("active");
     })
     gsharpKey.classList.add("active");
-    currentKey=7;
+    currentKey=8;
     generateScale(currentScale,currentKey);
     createPads();
     createPointer();
